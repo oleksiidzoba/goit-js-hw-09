94ce7e39117cfb95513638e38a52e95ad8eddcef
lexioner <dzobaoleksiy@gmail.com>
2026-02-16 02:52:42 +0200

    add form js,css,html

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+import "../css/form-styles.css";
+
+const formData = {
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", event => {
+  const { name, value } = event.target;
+
+  if (name === "email" || name === "message") {
+    formData[name] = value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
+});
